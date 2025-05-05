ad875ca2ad55e0a8105abbb1d2e1fd21f6df2610
Ivana Janoštíková <ivkagre@gmail.com>
2025-05-05 15:03:52 +0200

    Piskvorky Homework 4

--- hra.js
+++ hra.js
@@ -1,3 +1,5 @@
+import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
+
 let currentPlayer = 'circle';
 const gameButtons = document.querySelectorAll('.field button');
 
@@ -19,17 +21,30 @@ gameButtons.forEach((button) => {
     } else {
       playerIcon.src = 'images/cross.svg';
     }
+
+    let board = [];
+    gameButtons.forEach((btn) => {
+      if (btn.classList.contains('board__field--cross')) {
+        board.push('x');
+      } else if (btn.classList.contains('board__field--circle')) {
+        board.push('o');
+      } else {
+        board.push('_');
+      }
+    });
+
+    const winner = findWinner(board);
+    if (winner === 'x' || winner === 'o') {
+      alert('Vyhrál hráč se symbolem ' + winner + '.');
+      location.reload();
+    }
   });
 });
 
 const confirmLink = document.querySelector('.piskvorky__restart');
-
 confirmLink.addEventListener('click', (event) => {
   const clickRestart = confirm('Skutečně chceš hru restartovat?');
-
-  if (clickRestart === true) {
-    
-  } else {
+  if (!clickRestart) {
     event.preventDefault();
   }
 });
