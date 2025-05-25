edda5f27802516430b11416aa954a0c9c380b95a
Ivana Janoštíková <ivkagre@gmail.com>
2025-05-25 18:04:52 +0200

    AIplayer API ready

--- hra.js
+++ hra.js
@@ -3,6 +3,35 @@ import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 let currentPlayer = 'circle';
 const gameButtons = document.querySelectorAll('.field button');
 
+const playAiMove = async () =>{
+      let board = [];
+      gameButtons.forEach((btn) => {
+  if (btn.classList.contains('board__field--cross')) {
+    board.push('x');
+  } else if (btn.classList.contains('board__field--circle')) {
+    board.push('o');
+  } else {
+    board.push('_');
+  }
+    });
+
+    const response = await fetch('https://piskvorky.czechitas-podklady.cz/api/suggest-next-move',{
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      board: board,
+      player: 'x',
+    }),
+  });
+
+  const data = await response.json();
+  const { x, y } = data.position;
+  const index = x + y * 10;
+  gameButtons [index].click();
+  };
+
 gameButtons.forEach((button) => {
   button.addEventListener('click', (event) => {
     event.target.disabled = true;
@@ -21,7 +50,15 @@ gameButtons.forEach((button) => {
     } else {
       playerIcon.src = 'images/cross.svg';
     }
+  
+    /* vytváram funkciu po ťahu, ak zahrá krížik, nasleduje AI, zároveň oddeľujem reakciu používateľa a reakciu AI*/
 
+    if(currentPlayer === 'cross'){
+      playAiMove();
+    };
+
+    
+  
     let board = [];
     gameButtons.forEach((btn) => {
       if (btn.classList.contains('board__field--cross')) {
